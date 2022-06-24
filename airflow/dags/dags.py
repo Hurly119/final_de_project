0860b9cdeada90cd85f323cf00ff532367c9a38e
Hurly119 <hurlycabalan@yahoo.com>
2022-06-25 00:03:45 +0800

    git init

---
+++ airflow/dags/dags.py
@@ -0,0 +1,295 @@
+from datetime import timedelta
+import boto3
+import os
+from io import StringIO, BytesIO
+import feedparser
+import pandas as pd
+import spacy
+import requests
+import pybase64
+from google.cloud import bigquery, storage
+import shutil 
+
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+from airflow.decorators import task
+# from airflow.utils.dates import days_ago
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+# from airflow.providers.discord.operators.discord_webhook import DiscordWebhookOperator
+from datetime import datetime
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from utils import upload_formatted_rss_feed,scrape_reviews,scrape_appdetails,get_unique_appids,analyze_sentiment,label_polarity
+
+BUCKET_NAME = "news_sites"
+
+# Group directory in the bucket
+MY_FOLDER_PREFIX = "fem_hans"
+
+DATE_NOW = datetime.now().strftime("%Y-%m-%d")
+# Data directory for CSVs and OSM Images
+DATA_PATH = '/opt/airflow/data/'
+
+#################################################################
+################### 1.) EXTRACT #################################
+#################################################################
+@task(task_id="kotaku")
+def kotaku_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("kotaku","https://kotaku.com/rss")
+
+@task(task_id="escapist_mag")
+def escapist_mag_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("escapist_mag","https://www.escapistmagazine.com/v2/feed/")
+
+@task(task_id="eurogamer")
+def eurogamer_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("eurogamer","https://www.eurogamer.net/?format=rss")
+
+
+@task(task_id="gamespot_standard")
+def gamespot_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("gamespot","https://www.gamespot.com/feeds/mashup/")
+
+@task(task_id="indigames_plus")
+def indigames_plus_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("indigames_plus","https://indiegamesplus.com/feed")
+
+@task(task_id="ps_blog")
+def ps_blog_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("ps_blog","http://feeds.feedburner.com/psblog")
+
+
+@task(task_id="rock_paper_sg")
+def rock_paper_sg_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("rock_paper_sg","http://feeds.feedburner.com/RockPaperShotgun")
+
+@task(task_id="steam_news")
+def steam_news_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("steam_news","https://store.steampowered.com/feeds/news.xml")
+
+@task(task_id="ancient_gaming")
+def ancient_gaming_feed(ds=None, **kwargs):
+    upload_formatted_rss_feed("ancient_gaming","http://feeds.feedburner.com/TheAncientGamingNoob")
+
+
+@task(task_id="combine_all_articles")
+def combine_all_articles(ds=None,**kwargs):
+    files = os.listdir(f"{DATA_PATH}articles")
+    dfs = []
+    for file in files:
+        outfile = f"{DATA_PATH}articles/{file}"
+        if not outfile.endswith('.csv'):
+            continue
+        df = pd.read_csv(outfile)
+        dfs.append(df)
+    game_articles = pd.concat(dfs)
+
+    game_articles.to_csv(f"{DATA_PATH}game_articles_{DATE_NOW}.csv")
+
+@task(task_id="scrape_game_reviews")
+def scrape_game_reviews(ds=None,**kwargs):
+    game_articles = pd.read_csv(f"{DATA_PATH}game_articles_{DATE_NOW}.csv")
+    appids = get_unique_appids(game_articles)
+
+    all_reviews = []
+    for appid in appids:
+        reviews_list = scrape_reviews(appid)
+        all_reviews += reviews_list
+    game_reviews = pd.DataFrame(all_reviews)
+
+    game_reviews.to_csv(f"{DATA_PATH}game_reviews_{DATE_NOW}.csv")
+
+@task(task_id="scrape_game_details")
+def scrape_game_details(ds=None,**kwargs):
+    game_articles = pd.read_csv(f"{DATA_PATH}game_articles_{DATE_NOW}.csv")
+    appids = get_unique_appids(game_articles)
+    
+    lst_game_details = scrape_appdetails(appids)
+    
+    game_details = pd.DataFrame(lst_game_details).rename(columns={"appid":"appids"})
+
+    game_details.to_csv(f"{DATA_PATH}game_details_{DATE_NOW}.csv")
+
+#################################################################
+################### 2.) TRANSFORM ###############################
+#################################################################
+
+@task(task_id="word_count")
+def word_count(ds=None, **kwargs):
+
+    def word_count(text):
+        words = text.split()
+        freq = [words.count(w) for w in words]
+        word_dict = dict(zip(words, freq))
+        return word_dict
+
+
+    game_article_filename = f"{DATA_PATH}game_articles_{DATE_NOW}.csv"
+    game_reviews_filename = f"{DATA_PATH}game_reviews_{DATE_NOW}.csv"
+
+    game_articles = pd.read_csv(game_article_filename)
+    game_reviews = pd.read_csv(game_reviews_filename)
+        ################################# TODO: IMPORTANT #########################################
+        # you need to find the column where the text/content is located e.g. 'summary' or 'content'
+        # and add a conditional logic below
+        ###########################################################################################
+    print(f"getting word_count for game articles...")
+    game_articles['sum_word_cnt'] = game_articles['summary'].apply(lambda x: len(x.split()))
+    game_articles['dict_word_cnt'] = game_articles['summary'].apply(lambda x: word_count(x))
+        ###########################################################################################
+
+    game_articles.to_csv(game_article_filename, index=False)
+    print(f"word_count for game_articles collected!")
+    print()
+
+    print(f"getting word_count for game reviews...")
+    game_reviews['sum_word_cnt'] = game_reviews['review'].apply(lambda x: len(x.split()))
+    game_reviews['dict_word_cnt'] = game_reviews['review'].apply(lambda x: word_count(x))
+
+    game_reviews.to_csv(game_reviews_filename,index=False)
+    print(f"word_count for game reviews collected!")
+    print()
+    
+@task(task_id='sentiment_analysis')
+def sentiment_analysis(ds=None,**kwargs):
+    game_article_filename = f"{DATA_PATH}game_articles_{DATE_NOW}.csv"
+    game_reviews_filename = f"{DATA_PATH}game_reviews_{DATE_NOW}.csv"
+
+    game_articles = pd.read_csv(game_article_filename)
+    game_reviews = pd.read_csv(game_reviews_filename)
+
+    print("getting sentiment for articles...")
+    game_articles['polarity_score_summary'] = game_articles['summary'].apply(lambda x: analyze_sentiment(x))
+    game_articles["sentiment_summary"] = game_articles["polarity_score_summary"].apply(lambda x: label_polarity(x["compound"]))
+        
+    game_articles['polarity_score_title'] = game_articles['title'].apply(lambda x: analyze_sentiment(x))
+    game_articles["sentiment_title"] = game_articles["polarity_score_title"].apply(lambda x: label_polarity(x["compound"]))
+    game_articles.to_csv(game_article_filename, index=False)
+    print("sentiment for articles taken!")
+
+    print("getting sentiment for game reviews...")
+    game_reviews["sentiment"] = game_reviews["voted_up"].apply(lambda x: "positive" if x else "negative")
+    game_reviews.to_csv(game_reviews_filename,index=False)
+    print("sentiment for reviews taken!")
+# NER
+@task(task_id='spacy_ner')
+def spacy_ner(ds=None, **kwargs):
+    nlp = spacy.load("/model/en_core_web_sm/en_core_web_sm-3.3.0")
+    
+    def ner(text):
+        doc = nlp(text)
+        # print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+        # print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+        ner = {}
+        for entity in doc.ents:
+            ner[entity.text] = entity.label_
+            print(entity.text, entity.label_)
+        return ner
+
+    game_article_filename = f"{DATA_PATH}game_articles_{DATE_NOW}.csv"
+    game_reviews_filename = f"{DATA_PATH}game_reviews_{DATE_NOW}.csv"
+
+    game_articles = pd.read_csv(game_article_filename)
+    game_reviews = pd.read_csv(game_reviews_filename)
+        ################################# TODO: IMPORTANT #########################################
+        # you need to find the column where the text/content is located e.g. 'summary' or 'content'
+        # and add a conditional logic below
+        ###########################################################################################
+    print("getting NER for game articles...")
+    game_articles['NER'] = game_articles['summary'].apply(lambda x: ner(x))
+    game_articles.to_csv(game_article_filename,index=False)
+    print("getting NER for game articles complete!")
+    print()
+        ###########################################################################################
+    print("getting NER for game reviews...")
+    game_reviews["NER"] = game_reviews["review"].apply(lambda x: ner(x))
+    game_reviews.to_csv(game_reviews_filename)
+    print("getting NER for game reviews complete!")
+    print()
+
+with DAG(
+    'scrapers_proj_test',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        # 'depends_on_past': False,
+        # 'email': ['caleb@eskwelabs.com'],
+        # 'email_on_failure': False,
+        # 'email_on_retry': False,
+        # 'retries': 1,
+        # 'retry_delay': timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function,
+        # 'on_success_callback': some_other_function,
+        # 'on_retry_callback': another_function,
+        # 'sla_miss_callback': yet_another_function,
+        # 'trigger_rule': 'all_success'
+    },
+    description='Pipeline demo',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 6, 15),
+    catchup=False,
+    tags=['scrapers'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id="t1_start_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"starting task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+    t13 = BashOperator(
+        task_id="t13_start_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"starting task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+    t132 = BashOperator(
+        task_id="t132_start_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"starting task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+
+    t1_end = BashOperator(
+        task_id="t1_end_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"ending task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+
+    t2_end = BashOperator(
+        task_id="t2_end_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"ending task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+        
+    t3_end = BashOperator(
+        task_id="t3_end_msg",
+        bash_command = "echo 't1_end'",
+        # bash_command="curl -X POST -H 'Content-type: application/json' --data '{\"text\" : \"ending task 1: scraping\"}' \"https://discord.com/api/webhooks/986224448984195082/pQp4GNcVWh-J2XtmIycVnjxYuGRGVIYFeveDRS5EwvgmGozthyd_alj8wbeKhfVn9SSk/slack\"",
+        dag=dag
+    )
+
+    t1 >> [indigames_plus_feed()] >> combine_all_articles() >> t2_end >> \
+    [scrape_game_details(),scrape_game_reviews()] >> t13 >> [word_count(),spacy_ner(),sentiment_analysis()] >> t132
+    # t1 >> [indigames_plus_feed(),kotaku_feed(), escapist_mag_feed()] >> t13 \
+    # >> [eurogamer_feed(),ps_blog_feed(),gamespot_feed()] >> t132 \
+    # >> [steam_news_feed(),rock_paper_sg_feed(),ancient_gaming_feed()] \
+    # >> combine_all_articles() >> t1_end >> [scrape_game_details(),scrape_game_reviews()] \
+    # >> t2_end >> [sentiment_analysis(),spacy_ner(),word_count()] >> t3_end
+    # combine_all_articles() >> t1_end >> [scrape_game_details(),scrape_game_reviews()] >> t2_end
---
+++ airflow/dags/utils.py
@@ -0,0 +1,241 @@
+import requests
+import feedparser
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+from glob import glob
+
+import requests
+from bs4 import BeautifulSoup
+from dateutil import parser
+
+from datetime import datetime
+from math import isnan
+
+import spacy
+from airflow.models import Variable
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+import time
+import random
+
+import boto3
+
+
+MY_API_KEY = Variable.get("STEAM_API_KEY")
+
+def upload_string_to_gcs(csv_body, uploaded_filename, service_secret=os.environ.get('SERVICE_SECRET')):
+    BUCKET_NAME = "final-de-storage"
+    MY_FOLDER_PREFIX = "final_test"
+
+    gcs_resource = boto3.resource(
+        "s3",
+        region_name="auto",
+        endpoint_url="https://storage.googleapis.com",
+        aws_access_key_id=Variable.get("SERVICE_ACCESS_KEY"),
+        aws_secret_access_key=Variable.get("SERVICE_SECRET"),
+    )
+    gcs_resource.Object(BUCKET_NAME, MY_FOLDER_PREFIX + "/" + uploaded_filename).put(Body=csv_body.getvalue())
+
+
+def get_request(url,params=None,retries=0):
+    sleep_time = random.randint(1,10)
+    if retries >3:
+        print("Failed to get response cancelling...,")
+        return None
+    try:
+        response = requests.get(url=url,params=params)
+    except Exception as e:
+        retries +=1
+        return get_request(url,params,retries)
+        
+    if response:
+        return response.json()
+    else:
+        print("No Response. Retrying...")
+        time.sleep(sleep_time)
+        retries+=1
+        return get_request(url,params,retries)
+
+##initialize dfs
+def get_appid(game_name):
+    game_name = game_name.lower()
+    response = requests.get(url=f'https://store.steampowered.com/search/?term={game_name}&category1=998', headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    app_details = None
+    app_id = None
+    game_found = None
+    try:
+        app_details = soup.find(class_='search_result_row')
+        app_id = app_details['data-ds-appid']
+        game_found = app_details.find(class_="search_name").text.strip().lower()
+    except:
+        return None
+    
+    if game_name == game_found:
+        return app_id
+    return None
+
+def date_published_tostr(published):
+    return parser.parse(published).strftime("%Y-%m-%d %H:%M:%S")
+
+def init_appids(df):
+    df2 = df.copy()
+    tags = df2["tags"].values
+    source = df2["source"].iloc[0]
+    appids = []
+    for i,tag in enumerate(tags):
+        try:
+            if tag is None or isnan(tag):
+                appids.append(None)
+                print(f"{source}: game not found for:",i)
+                continue
+        except:
+            pass
+        ls_tag = None
+        try:
+            ls_tag = eval(tag)
+        except:
+            ls_tag = tag
+
+        for term in ls_tag:
+            game_name = term["term"]
+            appid = get_appid(game_name)
+            if appid:
+                appids.append(appid)
+                print(f"{source}: YAY",appid,game_name)
+                break
+        else:
+            appids.append(None)
+            print(f"{source}: game not found for:",i)
+    return appids
+
+def init_null_appids(nouns):
+    for noun in nouns:
+        appid = get_appid(noun)
+        print(f"checking if game \'{noun}\' on steam...")
+        if appid:
+            print(f"game \'{noun}\' found!.")
+            print()
+            return appid
+    print(f"article has no games on steam.")
+    print()
+    return None
+
+
+
+def get_nouns(title_summary):
+    nlp = spacy.load("/model/en_core_web_sm/en_core_web_sm-3.3.0")
+    stop_words = stopwords.words("english")
+    
+    summary = BeautifulSoup(title_summary,"html.parser").text.strip().lower()
+    doc = nlp(summary)
+    noun_phrases = set([noun.text for noun in doc.noun_chunks])
+    return [noun for noun in noun_phrases if noun not in stop_words]
+
+def upload_formatted_rss_feed(feed_name, feed):
+    DATA_PATH = '/opt/airflow/data/'
+    if not os.path.exists(f"{DATA_PATH}articles"):
+        os.mkdir(f"{DATA_PATH}articles")
+    feed = feedparser.parse(feed)
+    df = pd.DataFrame(feed['entries'])
+    columns = ["title","title_detail","link","links","author","authors","published","published_parsed","tags","id","guidislink","summary"]
+    df = df[columns]
+    df["source"] = feed_name
+    df = df_appids(df,feed_name)
+    time_now = datetime.now().strftime('%Y-%m-%d')
+    filename = feed_name + '_' + time_now + '.csv'
+    df.to_csv(f"{DATA_PATH}articles/{filename}", index=False)
+    print(f"df {filename} saved to .csv!")
+
+def df_appids(df2,feed_name):
+    df = df2.copy()
+
+    appids = init_appids(df)
+    print(f"getting appids for {feed_name}...")
+    df["appids"] = appids
+    df["summary"] = df["summary"].apply(lambda x: BeautifulSoup(x,"html.parser").text.strip().lower())
+    df["title_summary"] = df["title"] +" "+ df["summary"]
+    df["nouns"] = df["title_summary"].apply(get_nouns)
+    df["date_str"] = df["published"].apply(date_published_tostr)
+    df.loc[df["appids"].isnull(),"appids"]= df.loc[df["appids"].isnull(),"nouns"].apply(init_null_appids)
+    print(f"appids for {feed_name} initialized!")
+    return df
+
+def reviews_json_to_df(review_response,appid):
+    all_reviews = []
+    print(review_response)
+    for review in review_response["reviews"]:
+        author_data = review.pop("author")
+        review.update(author_data)
+        review.update({"appids":appid})
+        all_reviews.append(review)
+    return all_reviews
+
+def scrape_reviews(appid):
+    params = {'json' : 1,
+        'filter' : 'all',
+        'language' : 'english',
+        'review_type' : 'all',
+        'purchase_type' : 'all',
+        'day_range':1}
+
+    cursor = False
+    explored_cursors = []
+    reviews_list = []
+
+    while cursor not in explored_cursors:
+        explored_cursors.append(cursor)
+        have_cursor = True
+        reviews = get_request(f"https://store.steampowered.com/appreviews/{appid}",params=params)
+        try:
+            cursor = reviews["cursor"]
+
+            params["cursor"] = cursor.encode()
+        except:
+            have_cursor = False
+
+        params["num_per_page"] = 100
+        print("review count:",len(reviews_list))
+        print()
+        reviews_list += reviews_json_to_df(reviews,appid)
+
+        if not have_cursor: break
+    return reviews_list
+
+# def scrape_appdetails(appids):
+def scrape_appdetails(appids):
+    list_appdetails = []
+    for appid in appids:
+        print(f"getting appdetails for: {appid}")
+        app_detail = get_request(f"https://steamspy.com/api.php?request=appdetails&appid={appid}")
+        app_detail["tags"] = [app_detail["tags"]]
+        list_appdetails.append(app_detail)
+    return pd.DataFrame(list_appdetails)
+
+
+def get_unique_appids(game_articles):
+    non_null_appids = game_articles[game_articles["appids"].notnull()]
+    appids = non_null_appids["appids"].unique()
+    return appids
+
+def analyze_sentiment(sentence):
+    sid_obj = SentimentIntensityAnalyzer()
+    return sid_obj.polarity_scores(sentence)
+
+def label_polarity(polarity):
+    if polarity>=0.05:
+        return "positive"
+    elif polarity <=-0.05:
+        return "negative"
+    else:
+        return "neutral"
